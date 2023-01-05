26d053e9b0e821caf93c6750b0562b2b5e74082b
VelkovIv <ivaylo.velkov@gmail.com>
2023-01-05 19:35:45 +0200

    changes on JS code

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ var numberOfDrumButtons = document.querySelectorAll(".drum").length
 
 for (var i=0; i < numberOfDrumButtons; i++ ) {
 
-    document.querySelector("button").addEventListener("click", function () {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
         alert("I got clicked!");
-    }
+    });
     
-}
+} 
 
 
