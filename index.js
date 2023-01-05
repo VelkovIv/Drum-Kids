b0dc60ce1671323474b5f57e6727c97ce8f6722a
VelkovIv <ivaylo.velkov@gmail.com>
2023-01-05 22:35:41 +0200

    adding drums images in css

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var numberOfDrumButtons = document.querySelectorAll(".drum").length
 for (var i=0; i < numberOfDrumButtons; i++ ) {
 
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-        alert("I got clicked!");
+        var audio = new Audio("sounds/tom-1.mp3");
+        audio.play();
     });
     
 } 
